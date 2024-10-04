ffe9dfcb0bdfbf0bbf7fa1123b59a9dc985d5ec9
cheng xiong <chengjoindevit1997@gmail.com>
2024-10-04 16:42:44 +0700

    update pkg

--- src/app/(guest)/about/page.tsx
+++ src/app/(guest)/about/page.tsx
@@ -1,8 +1,48 @@
 export default function AboutPage() {
   return (
-    <div>
+    <div className="pt-12">
       <h1>About Page</h1>
       <span>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
+        maiores maxime porro eum nostrum excepturi illo voluptatem quis placeat
+        voluptatibus quasi, magni, fugit natus quo accusamus reiciendis dolorum
+        sapiente laboriosam. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Voluptatum maiores maxime porro eum nostrum excepturi illo
+        voluptatem quis placeat voluptatibus quasi, magni, fugit natus quo
+        accusamus reiciendis dolorum sapiente laboriosam. Lorem ipsum dolor sit
+        amet consectetur, adipisicing elit. Voluptatum maiores maxime porro eum
+        nostrum excepturi illo voluptatem quis placeat voluptatibus quasi,
+        magni, fugit natus quo accusamus reiciendis dolorum sapiente laboriosam.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
+        maiores maxime porro eum nostrum excepturi illo voluptatem quis placeat
+        voluptatibus quasi, magni, fugit natus quo accusamus reiciendis dolorum
+        sapiente laboriosam. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Voluptatum maiores maxime porro eum nostrum excepturi illo
+        voluptatem quis placeat voluptatibus quasi, magni, fugit natus quo
+        accusamus reiciendis dolorum sapiente laboriosam. Lorem ipsum dolor sit
+        amet consectetur, adipisicing elit. Voluptatum maiores maxime porro eum
+        nostrum excepturi illo voluptatem quis placeat voluptatibus quasi,
+        magni, fugit natus quo accusamus reiciendis dolorum sapiente laboriosam.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
+        maiores maxime porro eum nostrum excepturi illo voluptatem quis placeat
+        voluptatibus quasi, magni, fugit natus quo accusamus reiciendis dolorum
+        sapiente laboriosam. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Voluptatum maiores maxime porro eum nostrum excepturi illo
+        voluptatem quis placeat voluptatibus quasi, magni, fugit natus quo
+        accusamus reiciendis dolorum sapiente laboriosam. Lorem ipsum dolor sit
+        amet consectetur, adipisicing elit. Voluptatum maiores maxime porro eum
+        nostrum excepturi illo voluptatem quis placeat voluptatibus quasi,
+        magni, fugit natus quo accusamus reiciendis dolorum sapiente laboriosam.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
+        maiores maxime porro eum nostrum excepturi illo voluptatem quis placeat
+        voluptatibus quasi, magni, fugit natus quo accusamus reiciendis dolorum
+        sapiente laboriosam. Lorem ipsum dolor sit amet consectetur, adipisicing
+        elit. Voluptatum maiores maxime porro eum nostrum excepturi illo
+        voluptatem quis placeat voluptatibus quasi, magni, fugit natus quo
+        accusamus reiciendis dolorum sapiente laboriosam. Lorem ipsum dolor sit
+        amet consectetur, adipisicing elit. Voluptatum maiores maxime porro eum
+        nostrum excepturi illo voluptatem quis placeat voluptatibus quasi,
+        magni, fugit natus quo accusamus reiciendis dolorum sapiente laboriosam.
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
         maiores maxime porro eum nostrum excepturi illo voluptatem quis placeat
         voluptatibus quasi, magni, fugit natus quo accusamus reiciendis dolorum
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import Footer from "./_components/layouts/guest/footer";
+import Header from "./_components/layouts/guest/header";
+
 export default function Home() {
   return (
     <div>
+      <Header />
+      <div className="pt-12">
       <h1>Home Page</h1>
       <span className="tw-text-orange-400">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis esse
@@ -8,6 +13,8 @@ export default function Home() {
         consectetur molestias labore soluta placeat provident delectus obcaecati
         dolore non perferendis ex!
       </span>
+      </div>
+      <Footer/>
     </div>
   );
 }
